0f4a3e3df3ff1e591d6918e0ede71da0e887ab59
Alessandro Carbone <alessandro.carbone02@universitadipavia.it>
2023-05-18 12:50:35 +0200

    First version of the code that works

--- web/app.py
+++ web/app.py
@@ -1,30 +1,31 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
-max_score = 100
-test_name = "Python Challenge"
-students = [
-    {"name": "Sandrine",  "score": 100},
-    {"name": "Gergeley", "score": 87},
-    {"name": "Frieda", "score": 92},
-    {"name": "Fritz", "score": 40},
-    {"name": "Sirius", "score": 75},
-]
-
-@app.route("/results")
-def results():
-    context = {
-        "title": "Results",
-        "students": students,
-        "test_name": test_name,
-        "max_score": max_score,
-    }
-    return render_template("results.html", **context)
 
 @app.route("/")
 def home():
-    return render_template("base.html", title="ProvaAPP")
+    return render_template("provaJinja.html", title="Prova")
+
+
+with open("/home/ale/tesi/cli-apps-web-interface/files/stats-ridotto.csv", "r") as filecsv:
+    reader = csv.DictReader(filecsv, delimiter="\t")
+    header = reader.fieldnames
+    repositories = []
+    for data in reader:
+        repositories.append(data)
+
+
+@app.route("/data")
+def results():
+    context = {
+        "title": "RepoStats",
+        "header": header,
+        "repositories": repositories,
+    }
+    return render_template("data.html", **context)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
