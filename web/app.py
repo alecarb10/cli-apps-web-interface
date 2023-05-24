356e999463c893ba181ca84d8016864cf3e11fb4
Alessandro Carbone <alessandro.carbone02@universitadipavia.it>
2023-05-24 18:10:39 +0200

    Added web pages

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 
 app = Flask(__name__)
@@ -20,14 +20,23 @@ with open("/home/ale/tesi/cli-apps-web-interface/files/stats-ridotto.csv", "r") 
         repositories.append(data)
 
 
-@app.route("/data")
-def results():
+@app.route("/data", methods=["GET", "POST"])
+def stats():
     context = {
         "title": title,
         "header": header,
         "repositories": repositories,
     }
     return render_template("data.html", **context)
+
+
+like = 0
+dislike = 0
+@app.route("/vote", methods=["GET", "POST"])
+def button():
+    if request.method == "POST":
+        return render_template("vote.html", like=like+1, dislike=dislike+1)
+    return render_template("vote.html", like=like, dislike=dislike)
 
 
 if __name__ == "__main__":
