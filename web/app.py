25c0ad3a43ecc92346712e82f4379096e69d316e
Ale <ale@AleBuntu.myguest.virtualbox.org>
2023-05-29 18:46:35 +0200

    Updated code

--- web/app.py
+++ web/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import random
+
+from flask import Flask, render_template, request, make_response
 import csv
 
 app = Flask(__name__)
@@ -19,26 +21,52 @@ with open("/home/ale/tesi/cli-apps-web-interface/files/stats-ridotto.csv", "r") 
     for data in reader:
         repositories.append(data)
 
+def save(repositories):
+    with open("/home/ale/tesi/cli-apps-web-interface/files/stats-ridotto.csv", "w") as outfilecsv:
+        writer = csv.DictWriter(outfilecsv, delimiter="\t")
+        writer.writeheader()
+        for data in repositories[:5]:
+            writer.writerow(data)
+
 
 like = 0
 dislike = 0
-@app.route("/data", methods=["GET", "POST"])
+@app.route("/data")
 def stats():
     context = {
         "title": title,
         "header": header,
         "repositories": repositories,
     }
+    return render_template("data.html", **context)
+
+
+@app.route("/vote", methods=["GET", "POST"])
+def vote():
+    has_voted = False
+    vote_stamp = request.cookies.get('vote_stamp')
+
     if request.method == "POST":
-        return render_template("data.html", **context, like=like+1, dislike=dislike+1)
-    return render_template("data.html", **context, like=like+1, dislike=dislike+1)
+        has_voted = True
+        vote = request.form['vote']
+        if vote_stamp:
+            print(("You've already voted! The vote stamp is: " + vote_stamp))
+        else:
+            print("You can vote!")
+            vote_like = int(repositories['like'])
+            vote_dislike = int(repositories['dislike'])
+            vote_like += 1
+            vote_dislike += 1
+            save(repositories)
 
+    resp = make_response(render_template("vote.html"))
 
-# @app.route("/vote", methods=["GET", "POST"])
-# def button():
-#     if request.method == "POST":
-#         return render_template("vote.html", like=like+1, dislike=dislike+1)
-#     return render_template("vote.html", like=like, dislike=dislike)
+    if has_voted:
+        vote_stamp = hex(random.getrandbits(64))[2:-1]
+        print("Set cookie for voted")
+        resp.set_cookie("vote_stamp", vote_stamp)
+
+    return resp
 
 
 if __name__ == "__main__":
