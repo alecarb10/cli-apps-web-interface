d5ad3bdaca6b037494d5822bc27299caf4a8a563
alecarb10 <alessandro.carbone02@universitadipavia.it>
2023-06-08 18:18:43 +0200

    United in a unique method stats and vote

--- web/app.py
+++ web/app.py
@@ -8,10 +8,11 @@ with open("/home/ale/tesi/cli-apps-web-interface/files/stats-ridotto.csv", "r") 
     reader = csv.DictReader(filecsv, delimiter="\t")
     header = reader.fieldnames
     repositories = []
+    names = []
     for data in reader:
-        data['name']
         data['like']
         data['dislike']
+        names.append(data['name'])
         repositories.append(data)
 
 
@@ -38,21 +39,10 @@ def home():
     return render_template("home.html", title=title)
 
 
-@app.route("/data")
+@app.route("/data", methods=["GET", "POST"])
 def stats():
-    context = {
-        "title": title,
-        "header": header,
-        "repositories": repositories,
-    }
-    return render_template("data1.html", **context)
-
-
-@app.route("/vote", methods=["GET", "POST"])
-def vote():
     like = int(data['like'])
     dislike = int(data['dislike'])
-    fieldnames = header
 
     if request.method == "POST":
         if request.form.get('like') == 'LIKE':
@@ -64,20 +54,48 @@ def vote():
 
     context = {
         "title": title,
-        "name": data['name'],
+        "header": header,
+        "repositories": repositories,
         "like": like,
-        "dislike": dislike,
+        "dislike": dislike
     }
 
-    save(like, dislike, fieldnames)
-    resp = make_response(render_template("vote1.html", **context))
-
-    # if has_voted:
-    #     vote_stamp = hex(random.getrandbits(64))[2:-1]
-    #     print("Set cookie for voted")
-    #     resp.set_cookie("vote_stamp", vote_stamp)
+    save(like, dislike, header)
+    resp = make_response(render_template("data1.html", **context))
 
     return resp
+
+
+# @app.route("/vote", methods=["GET", "POST"])
+# def vote():
+#     like = int(data['like'])
+#     dislike = int(data['dislike'])
+#     fieldnames = header
+#
+#     if request.method == "POST":
+#         if request.form.get('like') == 'LIKE':
+#             like += 1
+#         elif request.form.get('dislike') == 'DISLIKE':
+#             dislike += 1
+#         else:
+#             pass
+#
+#     context = {
+#         "title": title,
+#         "names": names,
+#         "like": like,
+#         "dislike": dislike,
+#     }
+#
+#     save(like, dislike, fieldnames)
+#     resp = make_response(render_template("vote1.html", **context))
+#
+#     # if has_voted:
+#     #     vote_stamp = hex(random.getrandbits(64))[2:-1]
+#     #     print("Set cookie for voted")
+#     #     resp.set_cookie("vote_stamp", vote_stamp)
+#
+#     return resp
 
 
 if __name__ == "__main__":
