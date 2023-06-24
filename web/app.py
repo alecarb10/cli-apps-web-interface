5ebac77b49e23761ef5123eca2a83d3d02fd79e4
alecarb10 <alessandro.carbone02@universitadipavia.it>
2023-06-24 13:16:51 +0200

    Added sign up

--- web/app.py
+++ web/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, make_response, redirect, url_
 import csv
 import requests
 import json
-import time
+import base64
 
 app = Flask(__name__)
 
@@ -20,13 +20,20 @@ with open("/home/ale/tesi/cli-apps-web-interface/files/stats-ridotto.csv", "r") 
         repositories.append(data)
 
 
-def save(like, fieldnames):
+def save_like(like, fieldnames):
     with open("/home/ale/tesi/cli-apps-web-interface/files/stats-ridotto.csv", "w") as outfilecsv:
         writer = csv.DictWriter(outfilecsv, delimiter="\t", fieldnames=fieldnames)
         writer.writeheader()
         for data in repositories:
             data['like'] = like
             writer.writerow(data)
+
+
+def save_users(users):
+    with open("../files/users.csv", "a") as userscsv:
+        fieldnames = ['username', 'password', 'timestamp']
+        writer = csv.DictWriter(userscsv, delimiter="\t", fieldnames=fieldnames)
+        writer.writerow(users)
 
 
 title = "Cli Apps Web Interface"
@@ -42,7 +49,7 @@ def home():
     return render_template("home.html", title=title)
 
 
-@app.route("/data", methods=["GET", "POST"])
+@app.route("/data", methods=["GET", "POST"]) # Manca il salvataggio del numero di like
 def stats():
     i = 0
     for repo in repositories:
@@ -62,6 +69,41 @@ def stats():
     # print(context['repositories'])
     # save(repositories[i]['like'], header)
     resp = make_response(render_template("data1.html", **context))
+
+    return resp
+
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    message = ""
+
+    if request.method == "POST":
+        encoded_psw = base64.b64encode(request.form['password'].encode("utf-8"))
+
+        secretKey = open("../../recaptcha_private_key", "r").read()
+        captchaResponse = request.form.get("g-recaptcha-response")
+        userIP = request.remote_addr
+
+        captchaURL = f'''https://www.google.com/recaptcha/api/siteverify?secret={secretKey}&response={captchaResponse}&remoteip={userIP}'''
+
+        responseData = requests.get(captchaURL).text
+        parsedData = json.loads(responseData)
+
+        if parsedData['success'] == True:
+            message = "<p style='color: green;'>Your account has been submitted successfully!</p>"  # Show the user if reCAPTCHA is valid
+            return redirect(url_for('stats'))
+        else:
+            message = "<p style='color: red;'>Invalid reCAPTCHA</p>"  # Show error if the reCAPTCHA is invalid
+
+        save_users({"username": request.form['username'],
+                    "password": encoded_psw,
+                    "timestamp": parsedData['challenge_ts']})
+
+    context = {
+        "message": message,
+    }
+
+    resp = make_response(render_template("sign_up.html", **context))
 
     return resp
 
@@ -86,13 +128,16 @@ def login():
         else:
             message = "<p style='color: red;'>Invalid reCAPTCHA</p>"  # Show error if the reCAPTCHA is invalid
 
-
     error = None
 
     if request.method == "POST":
         if request.form['username'] != 'admin@admin.it' or request.form['password'] != 'admin':
             error = "Invalid Credentials. Please try again."
         else:
+            encoded_psw = base64.b64encode(request.form['password'].encode("utf-8"))
+            print(encoded_psw)
+            users = [request.form['username'], encoded_psw, 0]
+            save_users(users)
             return redirect(url_for('stats'))
 
     context = {
